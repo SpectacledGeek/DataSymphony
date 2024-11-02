b65380b4ecab91476b9b1e889d9b8e3c960fa27c
Suryansh Singh <suryanshsingh1807@gmail.com>
2024-11-02 18:09:17 +0530

    chapter Creation API

--- src/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/ChapterFrom.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/ChapterFrom.tsx
@@ -1,117 +1,129 @@
-"use client"
+"use client";
 
-import * as z from 'zod';
-import axios from 'axios';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
-import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form';
-import { Button } from '@/components/ui/button';
-import { Pencil } from 'lucide-react';
-import { useState } from 'react';
-import { toast } from 'sonner';
-import { useRouter } from 'next/navigation';
-import { cn } from '@/lib/utils';
-import { Textarea } from '@/components/ui/textarea';
-import { Course } from '@prisma/client';
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Pencil, PlusCircle } from "lucide-react";
+import { useState } from "react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { Chapter, Course } from "@prisma/client";
+import { Input } from "@/components/ui/input";
 
 const formSchema = z.object({
-    description: z.string().min(1, {
-        message: 'Description is required'
-    }),
-})
+  title: z.string().min(1),
+});
 type TformSchema = z.infer<typeof formSchema>;
 
 interface ChapterFormProps {
-    initialData: Course
-    courseId: string;
+  initialData: Course & { chapters: Chapter[] };
+  courseId: string;
 }
 
 const ChapterForm = ({ initialData, courseId }: ChapterFormProps) => {
-    const [isEditing, setIsEditing] = useState(false);
+  const [isCreating, setIsCreating] = useState(false);
+  const [isUpdating, setisUpdating] = useState(false);
 
-    const router = useRouter();
+  const router = useRouter();
 
-    const toggleEdit = () => {
-        setIsEditing(!isEditing);
+  const toggleCreating = () => {
+    setIsCreating(!isCreating);
+  };
+
+  const form = useForm<TformSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (data: TformSchema) => {
+    try {
+      const response = await axios.post(
+        `/api/courses/${courseId}/chapters`,
+        data
+      );
+      toast.success("Chapter Created");
+      toggleCreating();
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong. Please try again.");
+      console.error("Description Form: ", error);
     }
+  };
 
-    const form = useForm<TformSchema>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            description: initialData?.description || ""
-        }
-    });
-    const { isSubmitting, isValid } = form.formState;
+  return (
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Course Chapters
+        <Button onClick={toggleCreating} variant={"ghost"}>
+          {isCreating ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <PlusCircle className="h-4 w-4 mr-2" />
+              Add a Chapter
+            </>
+          )}
+        </Button>
+      </div>
 
-    const onSubmit = async (data: TformSchema) => {
-        try {
-            const response = await axios.patch(`/api/courses/${courseId}`, data);
-            toast.success('Course updated successfully');
-            toggleEdit();
-            router.refresh();
-        } catch (error) {
-            toast.error('Something went wrong. Please try again.');
-            console.error( "Description Form: " ,error);
-        }
-    }
-
-    return ( 
-        <div className='mt-6 border bg-slate-100 rounded-md p-4' >
-            <div className='font-medium flex items-center justify-between' >
-                Course Description
-                <Button onClick={toggleEdit} variant={"ghost"} >
-                    {isEditing ? (
-                        <>Cancel</>
-                    ) : (
-                        <>
-                            <Pencil className='h-4 w-4 mr-2' />
-                            Edit Description
-                        </>
-                    )}
-                </Button>
-            </div>
-            {!isEditing ? (
-                <div className={cn(
-                    "text-sm mt-2",
-                    !initialData.description && 'text-slate-500 italic'
-                )} >
-                    {initialData.description || "No description"}
-                </div>
-            ) : (
-                <Form {...form}>
-                    <form
-                        onSubmit={form.handleSubmit(onSubmit)}
-                        className='space-y-4 mt-4'
-                    >
-                        <FormField
-                            control={form.control}
-                            name="description"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormControl>
-                                        <Textarea 
-                                            disabled={isSubmitting}
-                                            placeholder="e.g. 'This course is about...'"
-                                            {...field}
-                                        />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <div className='flex items-center gap-x-2' >
-                            <Button
-                                disabled={!isValid || isSubmitting}
-                                type="submit"
-                            >
-                                Save
-                            </Button>
-                        </div>
-                    </form>
-                </Form>    
-            )}
+      {!isCreating && (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 mt-4"
+          >
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="e.g. 'Introduction to the Course'"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button disabled={!isValid || isSubmitting} type="submit">
+              Create
+            </Button>
+          </form>
+        </Form>
+      )}
+      {!isCreating && (
+        <div
+          className={cn(
+            "text-sm mt-2",
+            !initialData.chapters.length && "text-slate-500 italic"
+          )}
+        >
+          {!initialData.chapters.length && "No Chapters"}
         </div>
-    );
-}
- 
+      )}
+      {!isCreating && (
+        <p className=" text-xs text-muted-foreground mt-4">
+          Drag and Drop to reorder chapters
+        </p>
+      )}
+    </div>
+  );
+};
+
 export default ChapterForm;
--- src/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,7 +1,12 @@
 import { IconBadge } from "@/components/IconsBadge";
 import { db } from "@/lib/db";
-import { auth } from '@clerk/nextjs/server';
-import { CircleDollarSign, File, LayoutDashboard, ListCheck } from "lucide-react";
+import { auth } from "@clerk/nextjs/server";
+import {
+  CircleDollarSign,
+  File,
+  LayoutDashboard,
+  ListCheck,
+} from "lucide-react";
 import { redirect } from "next/navigation";
 import TitleForm from "./_components/TitleForm";
 import DescriptionForm from "./_components/DescriptionForm";
@@ -12,133 +17,107 @@ import AttachmentForm from "./_components/AttachmentForm";
 import ChapterForm from "./_components/ChapterFrom";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
-    const { userId } = auth()
-    if(!userId) {
-        return redirect('/')
-    }
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/");
+  }
 
-    const course = await db.course.findUnique({ 
-        where: { 
-            id: params.courseId
-        },
-        include: {
-            // chapters: {
-            //     orderBy: {
-            //         position: 'asc'
-            //     }
-            // },
-            attachments: {
-                orderBy: {
-                    createdAt: 'desc'
-                }
-            }
-        }
-    })
-    if(!course) {
-        return redirect('/')
-    }
-
-    const categories = await db.category.findMany({
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+    include: {
+      chapters: {
         orderBy: {
-            name: 'asc'
-        }
-    })
+          position: "asc",
+        },
+      },
+      attachments: {
+        orderBy: {
+          createdAt: "desc",
+        },
+      },
+    },
+  });
+  if (!course) {
+    return redirect("/");
+  }
 
-    const requiredFields = [
-        course.title,
-        course.description,
-        course.imageUrl,
-        course.price,
-        course.categoryId
-    ]
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
 
-    const totalFields = requiredFields.length
-    const completedFields = requiredFields.filter(Boolean).length
-    const completionText = `(${completedFields}/${totalFields})`
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId,
+    course.chapters.some((chapter) => chapter.isPublished),
+  ];
 
-    return ( 
-        <div className="p-6" >
-            <div className="flex items-center justify-between" >
-                <div className="flex flex-col space-y-2" >
-                    <div className="text-2xl font-medium" >
-                        Course Setup
-                    </div>
-                    <span className="text-sm text-slate-700" >
-                        Complete all fields {completionText}
-                    </span>
-                </div>
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16" >
-                <div>
-                    <div className="flex items-center gap-x-2" >
-                        <IconBadge icon={LayoutDashboard} />
-                        <h2 className="text-xl" >
-                            Customise your course
-                        </h2>
-                    </div>
-                    <TitleForm 
-                        initialData={course}
-                        courseId={course.id}
-                    />
-                    <DescriptionForm 
-                        initialData={course}
-                        courseId={course.id}
-                    />
-                    <ImageUploadForm
-                        initialData={course}
-                        courseId={course.id}
-                    />
-                    <CategoryForm 
-                        initialData={course}
-                        courseId={course.id}
-                        options={categories.map(category => (
-                            { label: category.name, value: category.id }
-                        ))}
-                    />
-                </div>
-                <div className="space-y-6" >
-                    <div>
-                        <div className="flex items-center gap-x-2" >
-                            <IconBadge icon={ListCheck} />
-                            <h2 className="text-xl" >
-                                Course Chapters
-                            </h2>
-                        </div>
-                        <div>
-                            <ChapterForm 
-                                initialData={course}
-                                courseId={course.id}
-                            />
-                        </div>
-                    </div>
-                    <div>
-                        <div className="flex items-center gap-x-2" >
-                            <IconBadge icon={CircleDollarSign} />
-                            <div className="text-xl" >
-                                Sell Your Course
-                            </div>
-                        </div>
-                        <PriceForm 
-                            initialData={course}
-                            courseId={course.id}
-                        />
-                    </div>
-                    <div>
-                        <div className="flex items-center gap-x-2" >
-                            <IconBadge icon={File} />
-                            <div className="text-xl" >
-                                Resources & Attachments
-                            </div>
-                        </div>
-                        <AttachmentForm
-                            initialData={course}
-                            courseId={course.id}
-                        />
-                    </div>
-                </div>
-            </div>
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+  const completionText = `(${completedFields}/${totalFields})`;
+
+  return (
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col space-y-2">
+          <div className="text-2xl font-medium">Course Setup</div>
+          <span className="text-sm text-slate-700">
+            Complete all fields {completionText}
+          </span>
         </div>
-    );
-}
- 
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={LayoutDashboard} />
+            <h2 className="text-xl">Customise your course</h2>
+          </div>
+          <TitleForm initialData={course} courseId={course.id} />
+          <DescriptionForm initialData={course} courseId={course.id} />
+          <ImageUploadForm initialData={course} courseId={course.id} />
+          <CategoryForm
+            initialData={course}
+            courseId={course.id}
+            options={categories.map((category) => ({
+              label: category.name,
+              value: category.id,
+            }))}
+          />
+        </div>
+        <div className="space-y-6">
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={ListCheck} />
+              <h2 className="text-xl">Course Chapters</h2>
+            </div>
+            <div>
+              <ChapterForm initialData={course} courseId={course.id} />
+            </div>
+          </div>
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={CircleDollarSign} />
+              <div className="text-xl">Sell Your Course</div>
+            </div>
+            <PriceForm initialData={course} courseId={course.id} />
+          </div>
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={File} />
+              <div className="text-xl">Resources & Attachments</div>
+            </div>
+            <AttachmentForm initialData={course} courseId={course.id} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default CourseIdPage;
